e811e0e9328e64383b0a125921c30625426be203
aladdinalizada <tu7c5zneb@code.edu.az>
2023-01-12 01:04:48 +0400

    completed hero section

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import image from "../cards/customer-service.png";
+// import image from "../cards/customer-service.png";
 import "./cards.scss";
 
 const CardsPropation = () => {
